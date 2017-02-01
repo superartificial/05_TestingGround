cf5cf5566c9624244f1e875c885ec01241825065
I5WIN7\user <clemw@hotmail.com>
2017-02-02 12:42:43 +1300

    Initial commit

---
+++ Source/S05_TestingGround/S05_TestingGroundGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "S05_TestingGroundGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AS05_TestingGroundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AS05_TestingGroundGameMode();
+};
+
+
+
