2657dffb79ef871bc3c2f7e251874e44b60a04e8
I5WIN7\user <clemw@hotmail.com>
2017-02-10 16:50:35 +1300

    Moved File System

--- Source/S05_TestingGround/S05_TestingGroundHUD.cpp
+++ Source/S05_TestingGround/S05_TestingGroundHUD.cpp
@@ -9,7 +9,7 @@
 AS05_TestingGroundHUD::AS05_TestingGroundHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 
